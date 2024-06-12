d369ddaf613dbeaa1aa54292a3185cd7ddaa891f
Eric L. Bentley <11730553+ebntly@users.noreply.github.com>
2024-06-11 20:30:07 -0500

    chore: migrate sub-modules from module. (#24) (#25)
    Merge changes from other branches before dev.

---
+++ types/constants.ts
@@ -0,0 +1,4 @@
+export const toBoolean = Symbol.for('toBoolean');
+export const toNumber = Symbol.for('toNumber');
+export const toBigInt = Symbol.for('toBigInt');
+export const toSymbol = Symbol.for('toSymbol');
--- version.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @copyright 2020-2024 integereleven. All rights reserved. MIT license.
- * @file Version of the module.
- */
-
-/**
- * Version of the module.
- */
-export const VERSION = '0.0.1';
